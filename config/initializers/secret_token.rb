de0d8f3a8dbb9d632b52352d3ddd968991f60acb
yas <ilyukevich.victor@gmail.com>
2011-01-30 16:11:08 +0200

    create empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KursachPoBibd::Application.config.secret_token = '94469aa495a35723fa2c0fbf9f6f75b298baec1af112ba36cb373e28c90638c569c65936519ea6ae50be121e89d6626d748707d15bf20d967bb5cd1ada1ec04b'
